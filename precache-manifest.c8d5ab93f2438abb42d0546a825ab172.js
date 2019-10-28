fca1efc7bead31dd5f1ccc5e7c410ed8780ded5e
cpscott1 <thacam18@gmail.com>
2019-10-27 23:39:29 -0500

    Updates

---
+++ precache-manifest.c8d5ab93f2438abb42d0546a825ab172.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f943d424de7527207b2d72aaca7461b5",
+    "url": "/monsters/index.html"
+  },
+  {
+    "revision": "975597637a29f07b47c4",
+    "url": "/monsters/static/css/main.1c581970.chunk.css"
+  },
+  {
+    "revision": "f20b13843bb3c2ed718a",
+    "url": "/monsters/static/js/2.cfd29923.chunk.js"
+  },
+  {
+    "revision": "975597637a29f07b47c4",
+    "url": "/monsters/static/js/main.9df80fc6.chunk.js"
+  },
+  {
+    "revision": "b08f6d7fc5fcb3b49104",
+    "url": "/monsters/static/js/runtime-main.fdd2b06c.js"
+  }
+]);
